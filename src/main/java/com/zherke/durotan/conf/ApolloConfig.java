2f127736ccbba2c9fba51e3fa4a39423c1be32f2
liuwenbiao <liuwenbiao@cposp.com>
2020-04-28 12:59:03 +0800

    提交Apollo初步整合 以及去掉无用依赖

--- src/main/java/com/zherke/durotan/conf/ApolloConfig.java
+++ src/main/java/com/zherke/durotan/conf/ApolloConfig.java
@@ -1,10 +1,13 @@
-package com.zherke.durotan.conf;
-
-import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@EnableApolloConfig({"application"})
-public class ApolloConfig {
-
-}
+//package com.zherke.durotan.conf;
+//
+//import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//@EnableApolloConfig({"TEST1.Durotan"})
+//public class ApolloConfig {
+//    @Value("${common_initiator}")
+//    private String commonInitiator;
+//
+//}
